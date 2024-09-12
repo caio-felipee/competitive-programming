34fff71af73e0314d62f096f137e283c98191a92
Caio <caiofeliperro@gmail.com>
2024-09-12 09:46:41 -0300

    algorithms(geometry): add rectangle

---
+++ algorithms/geometry/rectangle.cpp
@@ -0,0 +1,41 @@
+template<typename T>
+struct Rectangle {
+    Point<T> P, Q;
+    T b, h;
+
+    Rectangle(const Point<T>& p, const Point<T>& q) : P(p), Q(q)
+    {
+        b = max(P.x, Q.x) - min(P.x, Q.x);
+        h = max(P.y, Q.y) - min(P.y, Q.y);
+    }
+
+    Rectangle(const T& base, const T& height)
+        : P(0, 0), Q(base, height), b(base), h(height) {}
+
+    Rectangle intersection(const Rectangle& r) const
+    {
+        using interval = pair<T, T>;
+
+        auto I = interval(min(P.x, Q.x), max(P.x, Q.x));
+        auto U = interval(min(r.P.x, r.Q.x), max(r.P.x, r.Q.x));
+
+        auto a = max(I.first, U.first);
+        auto b = min(I.second, U.second);
+
+        if (b < a)
+            return { {-1, -1}, {-1, -1} };
+
+        I = interval(min(P.y, Q.y), may(P.y, Q.y));
+        U = interval(min(r.P.y, r.Q.y), may(r.P.y, r.Q.y));
+
+        auto c = max(I.first, U.first);
+        auto d = min(I.second, U.second);
+
+        if (d < c)
+            return { {-1, -1}, {-1, -1} };
+
+        auto inter = Rectangle(Point(a, c), Point(b, d));
+
+        return inter;
+    }
+};
